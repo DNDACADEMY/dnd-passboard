536162c91cf0c2de95ac538779d701a4cfe0e2a9
Zero-1016 <iolpd42@gmail.com>
2025-05-10 22:35:32 +0900

    feat: flex 컴포넌트 추가

---
+++ src/shared/components/Flex/Flex.tsx
@@ -0,0 +1,36 @@
+import { flexStyle } from './style.css'
+import { type CommonProps } from '@/shared/types/common'
+import { Slot } from '@radix-ui/react-slot'
+import { getGapSizeFromNumber } from './utils'
+import clsx from 'clsx'
+import { type FlexRecipeProps } from './style.css'
+
+export type FlexProps = FlexRecipeProps &
+  CommonProps & {
+    gap?: number
+    asChild?: boolean
+  }
+
+export const Flex = (props: FlexProps) => {
+  const {
+    direction,
+    align,
+    justify,
+    wrap,
+    asChild,
+    gap,
+    children,
+    className: classNameFromProps,
+    style: styleFromProps,
+    ...restProps
+  } = props
+  const Component = asChild ? Slot : 'div'
+  return (
+    <Component
+      style={{ gap: gap ? getGapSizeFromNumber(gap) : undefined, ...styleFromProps }}
+      className={clsx(flexStyle({ direction, align, justify, wrap }), classNameFromProps)}
+      {...restProps}>
+      {children}
+    </Component>
+  )
+}

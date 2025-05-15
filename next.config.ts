f97e221707467836da0e72e7c8f1072add52caa8
Zero-1016 <iolpd42@gmail.com>
2025-05-16 01:54:45 +0900

    chore: svg 컴포넌트를 만들기위한 설정을 추가해요

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,30 @@ const nextConfig: NextConfig = {
         hostname: '**' // Allow all remote patterns
       }
     ]
+  },
+  webpack(config) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'))
+
+    config.resolve.alias.canvas = false
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack']
+      }
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
   }
 }
 
